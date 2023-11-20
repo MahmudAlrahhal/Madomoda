32d0b0f9b1975c464799c74fc1b0df07d9df8679
MahmudAlrahhal <Mahmud@DESKTOP-8S8D5IM>
2023-11-21 01:22:39 +0300

    IRepository and ICategoryRepository interfaces, and their implementations were created and used in Category controller class.

---
+++ MadoDataAccess/Repository/CategoryRespository.cs
@@ -0,0 +1,30 @@
+﻿using MadoDataAccess.Data;
+using MadoDataAccess.Repository.IRepository;
+using MadoModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MadoDataAccess.Repository
+{
+    public class CategoryRespository : Repository<Category>, ICategoryRepository
+    {
+        private ApplicationDbContext _db;
+        // Repository Class required a 
+        public CategoryRespository(ApplicationDbContext _db) : base(_db)
+        {
+            this._db = _db;
+        }
+        public void save()
+        {
+            _db.SaveChanges();
+        }
+
+        public void update(Category category)
+        {
+            _db.Categories.Update(category);
+        }
+    }
+}
---
+++ MadoDataAccess/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,16 @@
+﻿using MadoModels;
+using Microsoft.EntityFrameworkCore.Query.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MadoDataAccess.Repository.IRepository
+{
+    public interface ICategoryRepository : IRepository<Category>
+    {
+        void update(Category category);
+        void save();
+    }
+}
